87bf1a77ccde5791e370f34cde606b3d41c763b4
verhovensky <105872686+verhovenskyivan@users.noreply.github.com>
2022-12-06 21:56:50 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.RegularExpressions;
+
+public class Kata
+{
+    public static string Shortcut(string input)
+    {
+        // TODO: Remove vowels
+        return Regex.Replace(input, "[aeiou]", "");
+    }
+}
+
